d79d44eb357e7cb63f6733c08a7062435c7abad4
Marc Bakker <webdev@marcbakker.com>
2020-03-22 00:26:53 +0100

    decouple the value displayed in the tile from the tile id.

--- src/components/tile.js
+++ src/components/tile.js
@@ -7,21 +7,31 @@ function Tile(props) {
 			type: props.tile.type,
 			row: props.tile.row,
 			col: props.tile.col,
-			id: props.tile.id
+			id: props.tile.id,
+			display: props.tile.display,
+			animation: props.tile.animation
 		});
 	};
 
-	const classNames = props.animate + " " + props.tile.type;
+	const classNames = [];
+	if (!!props.tile.animation) {
+		classNames.push(props.tile.animation);
+	}
+	if (props.tile.type === "emptyTile") {
+		classNames.push("emptyTile");
+	}
+	if (props.tile.type === "tile") {
+		classNames.push("tile");
+	}
+	console.log(classNames);
 	return (
 		<div
-			className={classNames}
+			className={classNames.join(" ")}
 			data-id={props.tile.id}
 			data-row={props.tile.row}
 			data-col={props.tile.col}
 			onClick={onClick}>
-			<img
-				src={props.tile.imgPath + "row-" + props.tile.row + "-col-" + props.tile.col + ".jpg"}
-				alt='tile'></img>
+			<p>{props.tile.display}</p>
 		</div>
 	);
 }
