f769219f23269708fea44f47bf96560956b5cd6a
Marc Bakker <webdev@marcbakker.com>
2020-03-31 17:03:49 +0200

    CSS: make room for text below subheader; I18n: change headertext.

--- src/Services/I18n.js
+++ src/Services/I18n.js
@@ -2,13 +2,13 @@ class I18n {
 	defaultLanguage = "nl";
 
 	static UITexts = {
-		HEADER_TEXT: { en: "Unscramble the puzzle!", nl: "Schuif de puzzel terug!" },
+		HEADER_TEXT: { en: "Unscramble the puzzle!", nl: "Los de puzzel op!" },
 		SUBHEADER_TEXT: { en: "Attempted moves:", nl: "Pogingen:" },
 		DISPLAYPROGRESSTEXT: { en: "Please wait... scrambling", nl: "Even wachten... husselen..." },
 		FINISHEDPROGRESSTEXT: { en: "Finished. Start unscrambling", nl: "Klaar Probeer de puzzel te maken" },
-		BUTTON_SCRAMBLE_LEVEL_1: { en: "Simple scramble", nl: "Beginnend puzzelaar" },
-		BUTTON_SCRAMBLE_LEVEL_2: { en: "Intermediate scramble", nl: "Gevorderd puzzelaar" },
-		BUTTON_SCRAMBLE_LEVEL_3: { en: "Pro scramble", nl: "Professionele puzzelaar" },
+		BUTTON_SCRAMBLE_LEVEL_1: { en: "Simple scramble", nl: "Eenvoudig" },
+		BUTTON_SCRAMBLE_LEVEL_2: { en: "Intermediate scramble", nl: "Gevorderd" },
+		BUTTON_SCRAMBLE_LEVEL_3: { en: "Pro scramble", nl: "Professioneel" },
 		BUTTON_SELECT_IMAGE_AMSTERDAM: { en: "Amsterdam", nl: "Amsterdam" },
 		BUTTON_SELECT_IMAGE_WORLD: { en: "Worldmap", nl: "Wereldkaart" }
 	};
