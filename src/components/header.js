570211e576784d62bfb56af0cf0e0d386de957bd
Marc Bakker <webdev@marcbakker.com>
2020-04-14 18:16:15 +0200

    remove commented code.

--- src/components/header.js
+++ src/components/header.js
@@ -11,23 +11,3 @@ const Header = ({ subHeaderText, moveCount }) => {
 	);
 };
 export { Header };
-
-// const Header = ({ updateHeaderReference, moveCount }) => {
-// 	const [subHeaderText, setSubHeaderText] = useState("");
-// 	const [_moveCount, setMoveCount] = useState(moveCount);
-
-// 	useEffect(() =>
-// 		updateHeaderReference({
-// 			updateHeaderText: text => setSubHeaderText(text),
-// 			updateMoveCount: count => setMoveCount(count)
-// 		})
-// 	);
-
-// 	return (
-// 		<header className='header'>
-// 			<p>{I18n.get("HEADER_TEXT")}</p>
-// 			<p className='subHeader'>{_moveCount ? `${subHeaderText} ${_moveCount}` : ""}</p>
-// 		</header>
-// 	);
-// };
-// export { Header };
